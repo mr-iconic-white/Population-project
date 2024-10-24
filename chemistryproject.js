67113169a71e25fb2149e6aa402111e7c9e92698
mr-iconic-white <saiprakashr25@gmail.com>
2024-10-24 22:39:01 +0530

    Add files via upload

---
+++ chemistryproject.js
@@ -0,0 +1,7 @@
+function showContent(sectionId) {
+    const sections = document.getElementsByClassName('content-section');
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+    document.getElementById(sectionId).style.display = 'block';
+}
